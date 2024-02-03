1131381eb891bd9684ca39eb17202f33e4636b48
afan <f2095522823@gmail.com>
2024-02-04 01:26:15 +0800

    :bug: fix: 修复 文件上传下载链接无协议头。

--- oss/domain/ossresp.py
+++ oss/domain/ossresp.py
@@ -1,8 +1,17 @@
 from ninja_schema import Schema
-from pydantic import Field
+from pydantic import Field, model_validator
 
 
 class OssResp(Schema):
     uploadUrl: str = Field(..., title="上传地址")
     downloadUrl: str = Field(..., title="下载地址")
 
+    @model_validator(mode="before")
+    def to_https(cls, values: dict) -> dict:
+        if isinstance(values, dict):
+            values["uploadUrl"] = values["uploadUrl"].replace("http://", "https://")
+            if values["downloadUrl"].startswith("http://"):
+                values["downloadUrl"] = values["downloadUrl"].replace("http://", "https://")
+            elif not values["downloadUrl"].startswith("https://"):
+                values["downloadUrl"] = "https://" + values["downloadUrl"]
+        return values
